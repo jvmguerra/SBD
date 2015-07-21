78f4f161934e1ad25477e7a5bf2a6a2caa57b8a4
Guilherme Oliveira <guilherme.061@gmail.com>
2015-07-21 00:26:13 -0300

    Create Realiza.sql

---
+++ Realiza.sql
@@ -0,0 +1,15 @@
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (1, 442);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (1, 443);
+
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (2, 441);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (2, 444);
+
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (3, 440);
+
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (4, 440);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (4, 441);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (4, 444);
+
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (5, 440);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (5, 441);
+INSERT INTO realiza (servico_realizado, funcionario) VALUES (5, 444);
