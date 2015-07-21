9827a519b4af7eb51bd3399b6ea1f4fde3cd271e
Guilherme Oliveira <guilherme.061@gmail.com>
2015-07-21 00:39:11 -0300

    Create Agenda_manutencao.sql

---
+++ Agenda_manutencao.sql
@@ -0,0 +1,20 @@
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (1, '97831433449', '06/15/2015', '06/15/2015', 442);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (1, '23245667299', '07/01/2015', '07/01/2015', 443);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (2, '13356369865', '04/15/2015', '04/18/2015', 441);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (2, '86529862153', '05/10/2015', '05/13/2015', 444);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (3, '06723742184', '03/02/2015', '03/07/2015', 440);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (3, '86883671080', '07/07/2015', '07/10/2015', 440);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (4, '12285585829', '02/21/2015', '02/22/2015', 444);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (4, '13356369865', '03/22/2015', '03/27/2015', 444);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (5, '82103582519', '05/14/2015', '05/13/2014', 441);
+INSERT INTO agenda_manutencao (servico_id, cliente_cpf, data_agendamento, data_concretizacao, funcionario_agendamento)
+		VALUES (5, '68220087218', '06/13/2015', '06/16/2015', 440);
