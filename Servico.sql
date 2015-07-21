a292da92ba201a9808a020a9f5ab7f559db4d86c
Guilherme Oliveira <guilherme.061@gmail.com>
2015-07-21 00:21:49 -0300

    Create Servico.sql

---
+++ Servico.sql
@@ -0,0 +1,5 @@
+INSERT INTO SERVICO(Tipo) VALUES('Serviço de Atendimento ao Cliente');
+INSERT INTO SERVICO(Tipo) VALUES('Reparaçao de Equipamentos');
+INSERT INTO SERVICO(Tipo) VALUES('Instalaçao de Equipamentos');
+INSERT INTO SERVICO(Tipo) VALUES('Troca de Equipamentos');
+INSERT INTO SERVICO(Tipo) VALUES('Recolhimento de Equipamento');
