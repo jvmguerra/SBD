c87cb1181545426ec65f1e9ad008922d75fa5cb0
Guilherme Oliveira <guilherme.061@gmail.com>
2015-07-20 23:06:05 -0300

    Update Empresa.sql

--- Empresa.sql
+++ Empresa.sql
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:Empresa.sql
 INSERT INTO Empresa VALUES ('49.184.261/0001-06', 'Setor Administrativo','Rua Amazônia','69900365','Rio de Janeiro','Brasil','99911428');
 INSERT INTO Empresa VALUES ('54.513.545/0001-20', 'Setor Financeiro','Rua Antônio Rocha Viana','69908560','Rio de Janeiro','Brasil','99780165');
 INSERT INTO Empresa VALUES ('44.819.446/0001-18', 'RH','Alameda Barcelona','69911250','Rio de Janeiro','Brasil','99783558');
@@ -9,47 +8,3 @@ INSERT INTO Empresa VALUES ('52.688.531/0001-12', 'Comercial 3','Rua Cunha Matos
 INSERT INTO Empresa VALUES ('94.860.585/0001-63', 'Setor Executivo','Rua Olivério José de Almeida','75106350','Uberlândia','Brasil','5757474');
 INSERT INTO Empresa VALUES ('21.515.052/0001-70', 'Sede','Rua Teodora de Mello','75106355','Rio de Janeiro','Brasil','2880091');
 INSERT INTO Empresa VALUES ('71.414.084/0001-58', 'Setor Tecnico','Rua I 4','75050720','Uberlândia','Brasil','2081444');
-=======
-#Funcionario
-
-INSERT INTO Funcionario VALUES ('110','Alessandro Batista Maia','99781067',
-	date(23/03/2014),160,1500.00,'84991176352','Rua Iracy Florêncio','55022756','Caruaru')
-INSERT INTO Funcionario VALUES ('111','Leandro Correia Couto','99981955',
-	date(12/10/2012),160,1700.00,'62865643140','Rua Jovelino Antônio','36047450','Juiz de Fora')
-INSERT INTO Funcionario VALUES ('112','Júlio Cerqueira Duarte','99980091',
-	date(02/10/2000),160,2400.00,'23963458267','Rua Marly Pinheiro','14098592','Ribeirão Preto')
-INSERT INTO Funcionario VALUES ('113','Guilherme Salvador Dias','99911517',
-	date(11/05/2010),160,1700.00,'67764741094','Rua Rio Branco','47813638','Barreiras')
-INSERT INTO Funcionario VALUES ('114','Mário Salvador Eiras','99982004',
-	date(11/11/2015),120,900.00,'73753398217',)
-INSERT INTO Funcionario VALUES ('220','Bárbara Manuela Carvalho','99988373',
-	date())
-INSERT INTO Funcionario VALUES ('221','Elisa Marina Catarina Barros','99919827',
-	date())
-INSERT INTO Funcionario VALUES ('222','Larissa Betina Sabrina','99987611',
-	date())
-INSERT INTO Funcionario VALUES ('223','Maitê Giovanna da Silva','99781428',
-	date())
-INSERT INTO Funcionario VALUES ('224','Jennifer Nina Raquel','99987611',
-	date())
-INSERT INTO Funcionario VALUES ('330','Alícia Mirella Rebeca','99369991',
-	date())
-INSERT INTO Funcionario VALUES ('331','Lorenzo Augusto Cavalcanti','99367731',
-	date())
-INSERT INTO Funcionario VALUES ('332','Juan Gustavo Ribeiro','99989827',
-	date())
-INSERT INTO Funcionario VALUES ('333','Letícia Mariah Betina','99988617',
-	date())
-INSERT INTO Funcionario VALUES ('334','Stefany Flávia Isabelle','99781620',
-	date())
-INSERT INTO Funcionario VALUES ('440','Evelyn Beatriz Mariane','99912999',
-	date())
-INSERT INTO Funcionario VALUES ('441','Anthony Raul Barbosa','99367611',
-	date())
-INSERT INTO Funcionario VALUES ('442','Caio Marcelo Ribeiro','99989827',
-	date())
-INSERT INTO Funcionario VALUES ('443','Danilo Fernando Lucas','99366617',
-	date())
-INSERT INTO Funcionario VALUES ('444','Yuri Renato Ian','99782115',
-	date())
->>>>>>> Stashed changes:Empresa.txt
