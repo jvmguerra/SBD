1fc96bba15aac0c73a4f2e79760135023006bf57
PedroVictorUFU <pedrovictorgf@gmail.com>
2015-07-22 00:41:16 -0300

    Update Dependente.sql

--- Dependente.sql
+++ Dependente.sql
@@ -1,10 +1,11 @@
-INSERT INTO Dependente VALUES (110,"Júlia Almeida Maia", "Esposa")
-INSERT INTO Dependente VALUES (110,"Miguel Almeida Maia", "Filho")
-INSERT INTO Dependente VALUES (222,"Francisco Martins Barão", "Marido")
-INSERT INTO Dependente VALUES (222,"Lucas Barão", "Filho")
-INSERT INTO Dependente VALUES (222,"Alice Sabrina Barão", "Filha")
-INSERT INTO Dependente VALUES (444,"Bernardo Victor Ian", "Filho")
-INSERT INTO Dependente VALUES (440,"Maria Marcia Mariane", "Filha")
-INSERT INTO Dependente VALUES (440,"Alice Beatriz Mariane", "Filha")
-INSERT INTO Dependente VALUES (330,"Amanda Rebeca", "Filha")
-INSERT INTO Dependente VALUES (332,"Cintia Pontes Maia", "Filho")
+
+INSERT INTO Dependente VALUES (110,'Júlia Almeida Maia', 'Esposa');
+INSERT INTO Dependente VALUES (110,'Miguel Almeida Maia', 'Filho');
+INSERT INTO Dependente VALUES (222,'Francisco Martins Barão', 'Marido');
+INSERT INTO Dependente VALUES (222,'Lucas Barão', 'Filho');
+INSERT INTO Dependente VALUES (222,'Alice Sabrina Barão', 'Filha');
+INSERT INTO Dependente VALUES (444,'Bernardo Victor Ian', 'Filho');
+INSERT INTO Dependente VALUES (440,'Maria Marcia Mariane', 'Filha');
+INSERT INTO Dependente VALUES (440,'Alice Beatriz Mariane', 'Filha');
+INSERT INTO Dependente VALUES (330,'Amanda Rebeca', 'Filha');
+INSERT INTO Dependente VALUES (332,'Cintia Pontes Maia', 'Filho');
