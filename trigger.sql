bd162b93798e0f327a63995ebaf3ef1c16122520
PedroVictorUFU <pedrovictorgf@gmail.com>
2015-07-22 04:10:29 -0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,3 +1,10 @@
+/*
+	Este trigger foi criado com o propósito de estar verificando se um cliente está tentando abrir duas ordens de
+	um mesmo serviço se já existe uma ordem anterior que ainda não foi finalizada. Quando o cliente tenta fazer o pedido
+	do serviço é realizada uma inserção na tabela agenda_manutencao. Quando ele for tentar realizar o pedido o trigger é
+	disparado e verifica se ja existe uma ordem para aquele serviço no nome do cliente se sim o trigger barra a inserção
+	caso contrario ele deixa a inserção ocorrer normalmente.
+*/
 CREATE OR REPLACE FUNCTION checa_servico ()
 RETURNS trigger AS $$
 DECLARE tupla record;
