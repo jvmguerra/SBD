2e60c895ee1873806be87dd831f8f8c140840589
Guilherme Oliveira <guilherme.061@gmail.com>
2015-07-22 04:14:33 -0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -9,11 +9,9 @@ CREATE OR REPLACE FUNCTION checa_servico ()
 RETURNS trigger AS $$
 DECLARE tupla record;
 BEGIN
-RAISE NOTICE 'X is %', new.cliente_cpf; 
 	FOR tupla IN SELECT * FROM Agenda_manutencao WHERE new.cliente_cpf = cliente_cpf AND new.servico_id = servico_id
 	LOOP
-		RAISE NOTICE 'Y is %', tupla.cliente_cpf;
-		IF tupla.Data_concretizacao = NULL THEN
+		IF tupla.realizado = false THEN
 		RAISE NOTICE 'Voce ainda possui uma manutenção pendente!';
 		RETURN NULL;
 		END IF;
